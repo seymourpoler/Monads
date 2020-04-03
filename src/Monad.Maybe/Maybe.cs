b941a6afd83430b6ac4924ace9966538e42eba77
seymourpoler <seymourpoler@gmail.com>
2020-04-03 16:30:23 +0200

    adding monad Optional

---
+++ src/Monad.Maybe/Maybe.cs
@@ -0,0 +1,88 @@
+using System;
+
+namespace Monad.Maybe
+{
+    public class Maybe<T>
+    {
+        private readonly T value;
+
+        public bool HasValue => value != null;
+
+        public static Maybe<T> None => new Maybe<T>(default(T));
+
+        private Maybe(T value)
+        {
+            this.value = value;
+        }
+        
+        public static Maybe<T> Of(T value)
+        {
+            return new Maybe<T>(value);
+        }
+
+        public void IfHasValue(Action<T> action)
+        {
+            if (action is null)
+            {
+                throw new ArgumentNullException();
+            }
+            if (HasValue){
+                action(value);
+            }
+        }
+        
+        public Maybe<TResult> Bind<TResult>(Func<T, TResult> function)
+        {
+            if (function is null)
+            {
+                throw new ArgumentNullException();
+            }
+
+            if (HasValue)
+            {
+                return Maybe<TResult>.Of(
+                    function.Invoke(value));    
+            }
+
+            return Maybe<TResult>.None;
+        }
+
+        public Maybe<TResult> Bind<TResult>(Func<T, Maybe<TResult>> function)
+        {
+            if (function is null)
+            {
+                throw new ArgumentNullException();
+            }
+
+            if (HasValue)
+            {
+                return function.Invoke(value);
+            }
+            return Maybe<TResult>.None;
+        }
+
+        public T IfNone(T result)
+        {
+            if (HasValue)
+            {
+                return value;    
+            }
+
+            return result;
+        }
+
+        public T IfNone(Func<T> function)
+        {
+            if (function is null)
+            {
+                throw new ArgumentNullException();
+            }
+            
+            if(HasValue){
+                return value;
+            }
+
+            return function.Invoke();
+        }
+    }
+}
---
+++ test/Monad.Maybe.Unit.Test/MaybeShould.cs
@@ -0,0 +1,193 @@
+using System;
+using Shouldly;
+using Xunit;
+
+namespace Monad.Maybe.Unit.Test
+{
+    public class MaybeShould
+    {
+        [Fact]
+        public void return_maybe_with_null_from_static_factory()
+        {
+            var result = Maybe<string>.Of(null);
+
+            result.ShouldBeOfType<Maybe<string>>();
+        }
+        
+        [Fact]
+        public void return_maybe_with_value_from_static_factory()
+        {
+            var result = Maybe<string>.Of("some value");
+
+            result.ShouldBeOfType<Maybe<string>>();
+        }
+
+        [Fact]
+        public void return_true_when_has_value()
+        {
+            var mayBe = Maybe<string>.Of("some value");
+
+            mayBe.HasValue.ShouldBeTrue();
+        }
+
+        [Fact]
+        public void return_with_no_value()
+        {
+            var result = Maybe<string>.None;
+
+            result.HasValue.ShouldBeFalse();
+        }
+
+        [Fact]
+        public void throw_exception_when_if_has_value()
+        {
+            var mayBe = Maybe<string>.Of("some value");
+
+            Action action = () => mayBe.IfHasValue(null);
+
+            action.ShouldThrow<ArgumentNullException>();
+        }
+        
+        [Fact]
+        public void execute_if_has_value()
+        {
+            var executed = false;
+            var mayBe = Maybe<string>.Of("some value");
+
+            mayBe.IfHasValue(x => executed = true);
+            
+            executed.ShouldBeTrue();
+        }
+        
+        [Fact]
+        public void not_execute_if_has_no_value()
+        {
+            var executed = false;
+            var mayBe = Maybe<string>.None;
+
+            mayBe.IfHasValue(x => executed = true);
+            
+            executed.ShouldBeFalse();
+        }
+
+        [Fact]
+        public void throws_exception_when_function_without_maybe_is_null()
+        {
+            var mayBe = Maybe<string>.Of("some value");
+            Func<string, bool> function = null;
+            Action action = () => { mayBe.Bind(function); };
+
+            action.ShouldThrow<ArgumentNullException>();
+        }
+        
+        [Fact]
+        public void bind_without_optional_when_has_value()
+        {
+            var mayBe = Maybe<string>.Of("some value");
+            
+            var result = mayBe.Bind(x => x.Contains("some"));
+
+            result.ShouldBeOfType<Maybe<bool>>();
+        }
+        
+        [Fact]
+        public void bind_without_optional_when_has_no_value()
+        {
+            var mayBe = Maybe<string>.None;
+            
+            var result = mayBe.Bind(x => new User());
+
+            result.ShouldBeOfType<Maybe<User>>();
+            result.HasValue.ShouldBeFalse();
+        }
+        
+        private class User{}
+        
+        [Fact]
+        public void throws_exception_when_function_with_maybe_is_null()
+        {
+            var mayBe = Maybe<string>.Of("some value");
+            Func<string, Maybe<bool>> function = null;
+            
+            Action action = () => { mayBe.Bind(function); };
+
+            action.ShouldThrow<ArgumentNullException>();
+        }
+        
+        [Fact]
+        public void bind_with_optional_when_has_value()
+        {
+            var mayBe = Maybe<string>.Of("some value");
+            
+            var result = mayBe.Bind(x => Maybe<bool>.Of(x.Contains("some")));
+
+            result.ShouldBeOfType<Maybe<bool>>();
+        }
+        
+        [Fact]
+        public void bind_with_optional_when_has_no_value()
+        {
+            var mayBe = Maybe<string>.None;
+            
+            var result = mayBe.Bind(x =>  Maybe<User>.Of(new User()));
+
+            result.ShouldBeOfType<Maybe<User>>();
+            result.HasValue.ShouldBeFalse();
+        }
+
+        [Fact]
+        public void return_value_when_has_value()
+        {
+            const string value = "some value";
+            var mayBe = Maybe<string>.Of(value);
+
+            var result = mayBe.IfNone("another value");
+            
+            result.ShouldBe(value);
+        }
+        
+        [Fact]
+        public void return_parameter_as_value_when_has_no_value()
+        {
+            const string value = "some value";
+            var mayBe = Maybe<string>.None;
+
+            var result = mayBe.IfNone(value);
+            
+            result.ShouldBe(value);
+        }
+
+        [Fact]
+        public void throw_exception_when_function_is_null()
+        {
+            var mayBe = Maybe<string>.None;
+            Func<string> function = null;
+
+            Action action = () => mayBe.IfNone(function);
+
+            action.ShouldThrow<ArgumentNullException>();
+        }
+
+        [Fact]
+        public void return_value_when_is_not_none()
+        {
+            const string value = "some thing";
+            var mayBe = Maybe<string>.Of(value);
+
+            var result = mayBe.IfNone(() => "another value");
+            
+            result.ShouldBe(value);
+        }
+        
+        [Fact]
+        public void return_value_from_function_when_is_none()
+        {
+            const string value = "some thing";
+            var mayBe = Maybe<string>.None;
+
+            var result = mayBe.IfNone(() => value);
+            
+            result.ShouldBe(value);
+        }
+    }
+}
